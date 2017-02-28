a8f2a3bd18c517514e038ad90f8c5b90b8070fa8
Juan Lopez <jc.lopez11@uniandes.edu.co>
2017-02-28 08:33:04 -0500

    Variable separation for efficiency

--- atoms.c
+++ atoms.c
@@ -7,7 +7,7 @@
 #define PI acos(-1.0)
 #define b 0.3
 
-void init(double *array, double *arrayp);
+void init(double *array, double *array_n, double *arrayp, double *arrayp_n);
 
 void main(int argc, char **argv){
 	int rank, source, destination, in_number, out_number;
@@ -20,33 +20,34 @@ void main(int argc, char **argv){
 	int t = 0;
 	int i;
 	double *x;
+	double *x_n;
 	double *xp;
+	double *xp_n;
 	double *ai;
 	double *ai1;
 	x = (double *)malloc(N*sizeof(double));
+	x_n = (double *)malloc(N*sizeof(double));
 	xp = (double *)malloc(N*sizeof(double));
+	xp_n = (double *)malloc(N*sizeof(double));
 	ai = (double *)malloc(N*sizeof(double));
 	ai1 = (double *)malloc(N*sizeof(double));
 	ai[0], ai[N-1], ai1[0], ai1[N-1] = 0.0;
-	init(x, xp);
+	init(x, x_n, xp, xp_n);
 	
 	while(t<T)
 	{
 		for(i=1;i<N-1;i++)
 		{
 			ai[i] = (x[i-1]+x[i+1]-2*x[i])+b*(pow(x[i+1]-x[i],3.0)-pow(x[i]-x[i-1],3));
+			x_n[i] = x[i]+xp[i]*dt+0.5*ai[i]*dt*dt;
+			ai1[i] = (x_n[i-1]+x_n[i+1]-2*x_n[i])+b*(pow(x_n[i+1]-x_n[i],3.0)-pow(x_n[i]-x_n[i-1],3));
+			xp_n[i] = xp[i]+0.5*(ai[i]+ai1[i])*dt;
 		}
-		for(i=1;i<N-1;i++)
+		
+		for(i=0;i<N;i++)
 		{
-			x[i] = x[i]+xp[i]*dt+0.5*ai[i]*dt*dt;
-		}
-		for(i=1;i<N-1;i++)
-		{
-			ai1[i] = (x[i-1]+x[i+1]-2*x[i])+b*(pow(x[i+1]-x[i],3.0)-pow(x[i]-x[i-1],3));
-		}
-		for(i=1;i<N-1;i++)
-		{
-			xp[i] = xp[i]+0.5*(ai[i]+ai1[i])*dt;
+			x[i] = x_n[i];
+			xp[i] = xp_n[i];
 		}
 		
 		if(t % 100 == 0)
@@ -65,11 +66,14 @@ void main(int argc, char **argv){
 	MPI_Finalize();
 }
 
-void init(double *array, double *arrayp){
+void init(double *array, double *array_n, double *arrayp, double *arrayp_n){
 	int i;
 	for(i=0;i<N;i++)
 	{
 		array[i] = sin(2*PI*i/((double)(N-1)));
+		array_n[i] = sin(2*PI*i/((double)(N-1)));
 		arrayp[i] = 0.0;
+		arrayp_n[i] = 0.0;
 	}
+	array[0], array[N-1] = 0.0;
 }
