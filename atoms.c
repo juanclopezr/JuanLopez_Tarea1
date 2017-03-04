8eb2f52843f791ba971d96d92adba3c5682ae9bc
Juan Barbosa <js.barbosa10@uniandes.edu.co>
2017-03-04 14:37:57 -0500

    minor changes

--- atoms.c
+++ atoms.c
@@ -12,42 +12,43 @@ double acceleration(int i, double *pos);
 
 int main(int argc, char **argv)
 {
-	int rank, source, destination, in_number, out_number;
-	
-	double dt = 5e-3, T = pow(5.0*N, 2.2), t = 0, a, a_;
+    int rank, source, destination, in_number, out_number;
+    double dt = 5e-3, T = pow(5.0*N, 2.2), t = 0, a, a_;
     int i, contador = 0, j = 0, n = (T/dt)/1000;
-	double *x, *vn, *vhalf;
-	x = (double *)malloc(N*sizeof(double));
-	vn = (double *)malloc(N*sizeof(double));
-	vhalf = (double *)malloc(N*sizeof(double));
-    
-	x, vn = init(x, vn);
-    T = T/10;
+    double *x, *vn, *vhalf;
+    x = (double *)malloc(N*sizeof(double));
+    vn = (double *)malloc(N*sizeof(double));
+    vhalf = (double *)malloc(N*sizeof(double));
+
+
+    double dt_half = 0.5*dt, dt_squared = 0.5*pow(dt, 2);
+    x, vn = init(x, vn);
     if (argc > 1)
     {
         omp_set_num_threads(atoi(argv[1]));
     }
     FILE *atpos = fopen("atpos.dat", "w");
-	
-	while(t<T)
-	{
+
+    while(t<T)
+    {
         #pragma omp parallel for private(a_)
-		for(i=1;i<N-1;i++)
-		{
+        for(i=1;i<N-1;i++)
+        {
             a_ = acceleration(i, x);
-			x[i] += vn[i]*dt + 0.5*a_*pow(dt, 2.0);
-            vn[i] += 0.5*dt*(acceleration(i, x)+a_);
+            x[i] += vn[i]*dt + a_*dt_squared;
+            vn[i] += dt_half*(acceleration(i, x)+a_);
         }
-		if(contador%n == 0)
-		{
-			for(i=0; i<N; i++)
-			{
-				fprintf(atpos,"%f\t", x[i]);
-			}
-			fprintf(atpos,"\n");
+        if(contador%n == 0)
+        {
+            //printf("%d\n", j);
+            for(i=0; i<N; i++)
+            {
+                fprintf(atpos,"%f\t", x[i]);
+            }
+            fprintf(atpos,"\n");
             j += 1;
-		}
-		t += dt;
+        }
+        t += dt;
         contador ++;
 	}
     fclose(atpos);
@@ -55,13 +56,13 @@ int main(int argc, char **argv)
 }
 
 double *init(double *pos, double *v){
-	int i;
-	for(i=0;i<N;i++)
-	{
-		pos[i] = sin(PI*i/((double)(N-1)));
+    int i;
+    for(i=0;i<N;i++)
+    {
+        pos[i] = sin(PI*i/((double)(N-1)));
         v[i] = 0;
-	}
-	pos[0], pos[N-1] = 0.0;
+    }
+    pos[0], pos[N-1] = 0.0;
     return pos, v;
 }
 
--- makefile
+++ makefile
@@ -5,12 +5,12 @@ graph.pdf time.pdf : atpos.dat graph.py times.dat timer.py
 
 atpos.dat times.dat : a.out
     # real, user, sys
-	time -o times.dat -f "%e \t%U \t%S" ./a.out 1
-	time -a -o times.dat -f "%e \t%U \t%S" ./a.out 2
-	time -a -o times.dat -f "%e \t%U \t%S" ./a.out 4
+	/usr/bin/time -o times.dat -f "%e \t%U \t%S" ./a.out 1
+	/usr/bin/time -a -o times.dat -f "%e \t%U \t%S" ./a.out 2
+	/usr/bin/time -a -o times.dat -f "%e \t%U \t%S" ./a.out 4
 
 a.out : atoms.c
-	gcc -lm -fopenmp atoms.c
+	gcc -lm -fopenmp -O3 atoms.c
 
 clean :
 	rm a.out atpos.dat graph.pdf time.pdf times.dat
